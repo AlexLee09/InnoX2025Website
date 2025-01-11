2ce4d4b2514c610162b5864e8fd0dcef525c5e66
AlexLee09 <alexandrekmlee@gmail.com>
2025-01-11 17:48:34 +1100

    layout setup

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,9 +4,12 @@ import NavItems from './NavItems'
 const Header = () => {
   return (
     <header className='w-full border-b h-[64px]'>
-        <div className='wrapper flex items-center justify-between'>
-            <NavItems>
-            </NavItems>
+        <div className='flex flex-row items-center justify-between px-40 bg-red-50 h-full '>
+            <h1>InnovationX</h1>
+            <div className='wrapper flex items-center justify-between'>
+                <NavItems>
+                </NavItems>
+            </div>
         </div>
     </header>
   )
--- constants/index.ts
+++ constants/index.ts
@@ -4,10 +4,6 @@ export const navLinks = [
         route: "/"
     },
     {
-        name: "About Us",
-        route: "/aboutus"
-    },
-    {
         name: "Mathematics",
         route: "/math"
     },
@@ -18,5 +14,28 @@ export const navLinks = [
     {
         name: "Economics",
         route: "/economics"
+    },
+    {
+        name: "About Us",
+        route: "/aboutus"
     }
 ];
+
+export const competitionInfo = [
+    {
+        title: "InnovationMath",
+        content: "Here is a resource package and look at the rubric",
+        formLink: "",
+
+    },
+    {
+        title: "InnovationTech",
+        content: "",
+        formLink: ""
+    },
+    {
+        title: "InnovatioEcons",
+        content: "resoruce package",
+        formLink: ""
+    }
+]
